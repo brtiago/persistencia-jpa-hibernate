7139528dae882531e284e2d533ea5332e5be4d85
Tiago <727320+brtiago@users.noreply.github.com>
2023-08-27 19:36:45 -0300

    Refactor: Adicionando enum Categoria

--- src/main/java/br/com/tiago/testes/CadastroDeProduto.java
+++ src/main/java/br/com/tiago/testes/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package br.com.tiago.testes;
 
 import br.com.tiago.dao.ProdutoDao;
+import br.com.tiago.modelo.Categoria;
 import br.com.tiago.modelo.Produto;
 import br.com.tiago.util.JPAUtil;
 import jakarta.persistence.EntityManager;
@@ -9,10 +10,7 @@ import java.math.BigDecimal;
 
 public class CadastroDeProduto {
     public static void main(String[] args) {
-        Produto celular = new Produto();
-        celular.setNome("Xiaomi redmi");
-        celular.setDescricao("Muito legal");
-        celular.setPreco(new BigDecimal(800));
+        Produto celular = new Produto("Xiaomi redmi", "Muito legal", new BigDecimal(800), Categoria.CELULARES);
 
         EntityManager em = JPAUtil.getEntityManager();
         ProdutoDao dao = new ProdutoDao(em);
